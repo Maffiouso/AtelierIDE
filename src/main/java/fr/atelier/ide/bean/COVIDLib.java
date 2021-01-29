1cc99f005776ba3ddc0a755f076339b46fbfc008
Maffiouso <theodore.matthieu@gmail.com>
2021-01-29 12:06:08 +0100

    Changement de nom de variable

--- src/main/java/fr/atelier/ide/bean/COVIDLib.java
+++ src/main/java/fr/atelier/ide/bean/COVIDLib.java
@@ -14,15 +14,15 @@ public class COVIDLib {
 
     public static void main(String[] args) {
 
-        COVIDLib app = new COVIDLib();
+        COVIDLib application = new COVIDLib();
 
         NormalDistribution normalDistribution = new NormalDistribution(15, 7);
 
-        System.out.println("Covid 7 jours : "+app.covidChanceEnMoyenne(normalDistribution, 15));
-        System.out.println("Covid entre 8 et 22 jours : "+app.covidChanceDurree(normalDistribution, 8, 22));
-        System.out.println("Covid entre 1 et 29 jours : "+app.covidChanceDurree(normalDistribution, 1, 29));
-        System.out.println("Covid entre 0 et 36 jours : "+app.covidChanceDurree(normalDistribution, -6, 29));
-        System.out.println("Covid 0 jours : "+app.covidChanceEnMoyenne(normalDistribution, 0));
+        System.out.println("Covid 7 jours : "+application.covidChanceEnMoyenne(normalDistribution, 15));
+        System.out.println("Covid entre 8 et 22 jours : "+application.covidChanceDurree(normalDistribution, 8, 22));
+        System.out.println("Covid entre 1 et 29 jours : "+application.covidChanceDurree(normalDistribution, 1, 29));
+        System.out.println("Covid entre 0 et 36 jours : "+application.covidChanceDurree(normalDistribution, -6, 29));
+        System.out.println("Covid 0 jours : "+application.covidChanceEnMoyenne(normalDistribution, 0));
 
     }
 
